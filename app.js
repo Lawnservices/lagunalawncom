991309c6943c5160b79f0cb15833a8418f700fc9
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-04-05 18:57:41 -0400

    fix-web

---
+++ app.js
@@ -0,0 +1,19 @@
+function laguna(){
+document.getElementById('mass').style.color = 'green';
+document.getElementById('mass').style.float = 'left';
+if(
+   mass.style.marginLeft= '20px',
+   mass.style.fontFamily = 'Courier'
+   
+   );
+     else{
+       mass.style.marginLeft= '20px',
+       mass.style.color = '#299684'
+     }; 
+}
+laguna();
+ 
+ 
+
+
+ 
