6298050f948f2c4c67259b693e54af6c55befcae
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-11-12 18:47:54 -0500

    fix

--- app.js
+++ app.js
@@ -1,17 +1,3 @@
-function laguna(){
-document.getElementById('mass').style.color = 'green';
-document.getElementById('mass').style.float = 'left';
-if(
-   mass.style.marginLeft= '20px',
-   mass.style.fontFamily = 'Courier'
-   
-   );
-     else{
-       mass.style.marginLeft= '20px',
-       mass.style.color = '#299684'
-     }; 
-}
-laguna();
  
  
 
